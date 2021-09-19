d95692d6e03290f245ed61613d556184efe91eb8
Nicko <quiamconicko@gmail.com>
2021-09-19 18:54:34 +0800

    update

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,13 +1,13 @@
-import { LocalStorage } from "quasar";
+// import { LocalStorage } from "quasar";
 
-export default ({ router }) => {
-  router.beforeEach((to, from, next) => {
-    let logged_in = LocalStorage.getItem('logged_in');
-    if(!logged_in && to.path !== '/auth'){
-      next('/auth');
-    }else{
-      next();
-    }
-    next();
-  })
-}
+// export default ({ router }) => {
+//   router.beforeEach((to, from, next) => {
+//     let logged_in = LocalStorage.getItem('logged_in');
+//     if(!logged_in && to.path !== '/auth'){
+//       next('/auth');
+//     }else{
+//       next();
+//     }
+//     next();
+//   })
+// }
