6296b12a8b2972c0eb8d0465f7ec290d6e9ead4b
Nicko <quiamconicko@gmail.com>
2021-11-28 01:40:21 +0800

    push

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCZlQSM_HAPn-uFjjEed0IRa1IopQipw28",
-  authDomain: "todo-list-38cdc.firebaseapp.com",
-  databaseURL: "https://todo-list-38cdc-default-rtdb.firebaseio.com",
-  projectId: "todo-list-38cdc",
-  storageBucket: "todo-list-38cdc.appspot.com",
-  messagingSenderId: "705142529190",
-  appId: "1:705142529190:web:67f04e0bcc17a39445a867"
+  // your firebase config here
 }
 
 let firebaseApp = firebase.initializeApp(firebaseConfig)
